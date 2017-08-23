f604c831547274fde4021527855953d83437981a
GaboMiranda <xlmirandalx@hotmail.com>
2017-08-23 15:30:27 -0500

    23/08/17-15:30

--- usuarios.sql
+++ usuarios.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: localhost
--- Tiempo de generación: 11-08-2017 a las 17:06:14
+-- Tiempo de generación: 23-08-2017 a las 20:29:05
 -- Versión del servidor: 10.1.25-MariaDB
 -- Versión de PHP: 7.1.7
 
@@ -37,17 +37,18 @@ CREATE TABLE `usuarios` (
   `password` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
   `tel` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `correo` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `nivel` int(11) NOT NULL
+  `nivel` int(11) NOT NULL,
+  `estatus` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `a_paterno`, `a_materno`, `usuario`, `password`, `tel`, `correo`, `nivel`) VALUES
-(1, 'gabriel', 'miranda', 'aviles', 'gmiranda', '123', '7777777777', 'asd@asd.com', 1),
-(3, 'karla', 'gonzález', 'sánchez', 'kgonzalez', '1234', '1111111111', 'karla@karla.com', 0),
-(4, 'algo', 'algo', 'algo', 'algo', '123', '123213', 'asdas', 0);
+INSERT INTO `usuarios` (`id`, `nombre`, `a_paterno`, `a_materno`, `usuario`, `password`, `tel`, `correo`, `nivel`, `estatus`) VALUES
+(1, 'gabriel', 'miranda', 'aviles', 'gmiranda', '123', '7777777777', 'asd@asd.com', 1, 1),
+(3, 'karla', 'gonzález', 'sánchez', 'kgonzalez', '1234', '1111111111', 'karla@karla.com', 0, 0),
+(4, 'algo', 'algo', 'algo', 'algo', '123', '123213', 'asdas', 0, 0);
 
 --
 -- Índices para tablas volcadas
@@ -67,7 +68,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;COMMIT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
